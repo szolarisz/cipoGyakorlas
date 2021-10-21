ededeb180537eb32bcc703e5069a0c1b1cf09707
szlevente05 <szurkelevente21@gmail.com>
2021-10-21 14:16:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 4444;
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    console.log(req.method);
+
+    switch (true) {
+        case req.url === "/" && req.method === "GET":
+            fs.readFile("./views/index.html", (err, file) => {
+                res.setHeader("Content-Type", "text/html; charset=UTF-8");
+                res.writeHead(200);
+                res.end(file);
+            });
+            break;
+
+        case req.url === "/favicon.ico" && req.method === "GET":
+            fs.readFile('./favicon.ico', (err, file) => {
+                res.setHeader('Content-Type', 'image/ico');
+                res.writeHead(200);
+                res.end(file);
+            });
+            break;
+        case req.url === "/ferfi" && req.method === "GET":
+            fs.readFile('./images/ferficipo.jpg', (err, file) => {
+                res.setHeader('Content-Type', 'image/jpg');
+                res.writeHead(200);
+                res.end(file);
+            });
+            break;
+        case req.url === "/noi" && req.method === "GET":
+            fs.readFile('./images/noicipo.jpg', (err, file) => {
+                res.setHeader('Content-Type', 'image/jpg');
+                res.writeHead(200);
+                res.end(file);
+            });
+            break;
+        case req.url === "/cipok" && req.method === "GET":
+            fs.readFile('./data/cipok.json', (err, file) => {
+                res.setHeader('Content-Type', 'application/json');
+                res.writeHead(200);
+                res.end(file);
+            });
+            // console.log("JSON feladva");
+            break;
+        case req.url === "/script.js" && req.method === "GET":
+            fs.readFile('./public/script.js', (err, file) => {
+                res.setHeader('Content-Type', 'application/js');
+                res.writeHead(200);
+                res.end(file);
+            });
+            break;
+
+        default:
+            fs.readFile("/views/hiba.html", (err, file) => {
+                res.setHeader('content-Type', 'text/html;charset=UTF-8');
+                res.writeHead(404);
+                res.end(file);
+            });
+            break;
+    }
+
+});
+
+server.listen(port);
---
+++ public/script.js
@@ -0,0 +1,31 @@
+//document.getElementById('cipok').innerHTML = feltolt();
+
+feltolt();
+
+async function feltolt() {
+    const valasz = await fetch("/cipok");
+    const cipoim = await valasz.json();
+    
+    let cipo2HTML = "";
+    for (egyCipo of cipoim) {
+        cipo2HTML += '<div class="card" style="width: 240px">';
+        cipo2HTML += `<img class="card-img-top" src="/${egyCipo.tipus}" alt="${egyCipo.tipus}cipo">`;
+        cipo2HTML += `<div class="card-body"><h5 class="card-title">${egyCipo.gyarto} ${egyCipo.meret}</h5>`;
+        cipo2HTML += `<p class="card-text">${egyCipo.leiras}<br>${egyCipo.ar}Ft</p>`;
+        cipo2HTML += `<a href="${egyCipo.honlap}" class="btn btn-primary">A gyártó honlapja</a>`;
+        cipo2HTML += `</div></div>`;
+    }
+    console.log(cipo2HTML);
+    document.getElementById('cipok').innerHTML = cipo2HTML;
+}
+
+/*----------------------------------------------------------------
+{
+        "tipus":"noi",
+        "meret":"38",
+        "gyarto":"Salamander",
+        "leiras":"Kényelmes őszi viselet",
+        "honlap":"https://salamander.hu",
+        "ar":"29990"
+    }
+*/
